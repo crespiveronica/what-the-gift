de1134700485ddeed0dd9d07e576045d6b2cddbf
fernandonino <nino.fernando@gmail.com>
2013-08-14 23:52:25 -0300

    Working homepage...

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Dfbengine::Application.routes.draw do
+  root to: 'static_pages#home'
+
   match '/projects', to: 'static_pages#projects', via: 'get'
   match '/about',    to: 'static_pages#about',    via: 'get'
   match '/contact',  to: 'static_pages#contact',  via: 'get'
@@ -56,7 +58,7 @@ Dfbengine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  # root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
