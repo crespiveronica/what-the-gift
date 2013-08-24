13980d6ec1bde52ca64e1ca11043556fbd9d6150
slifszyc <sacha.lifszyc@gmail.com>
2013-08-24 15:56:47 -0300

    Removed all Fer files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-WhatTheGift::Application.config.secret_token = 'b371879347235c3ad8819ce3e98ec1498c8bab22108bd25b6e85041a1de6c9485d509c60219cdc375042bd3904eb8d59080e577e0d0f636703e8ac0ffe691c01'
