3b9cd6cad94cd18d4dcea6d09f7eb5990d11f728
slifszyc <sacha.lifszyc@gmail.com>
2013-08-31 19:49:20 -0300

    Confirmar solicitud de amistad
    Herencia de los templates con layouts y la function inside_layout en application_helper.
    Modificado el .gitignore para que no tome los .DS_Store
    Agregadas las rutas necesarias

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,9 +1,17 @@
 class FriendsController < ApplicationController
 
   def index
+  	render 'friends/index', layout: 'myfriends'
   end
 
   def search
   end
 
+  def requests
+  	render 'friends/requests', layout: 'myfriends'
+  end
+
+  def pending
+  	render 'friends/pending', layout: 'myfriends'
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,9 @@ module ApplicationHelper
       "#{base_title} | #{page_title}"
     end
   end
+
+  def inside_layout(parent_layout)
+	  view_flow.set :layout, capture { yield }
+	  render template: "layouts/#{parent_layout}"
+	end
 end
