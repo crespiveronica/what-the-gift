48934cdc1f840a338580a7f5e2289f93d8040bd3
Fernando Nino <nino.fernando@gmail.com>
2013-09-08 18:58:45 -0300

    User populate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Users
+print "Creating 100 users..."
+
+ban_reasons = ["Vendía droga", "Traficaba órganos"]
+100.times do
+  u = User.new(
+    :first_name = Faker::Name.first_name,
+    :last_name = Faker::Name.last_name,
+    :email = Faker::Internet.email,
+    :password = "password",
+    :active = true,
+    :banned = false,
+    :banned_reason = ban_reasons.sample
+    # Para completar más luego... tiene referencias
+    # a productos que todavía no existen
+    #:whishlist,
+    #:received_gifts,
+    #:hobbies
+  )
+
+  u.save
+end
+puts "done."
+
+
+# Sellers
+print "Creating 50 sellers..."
+
+puts "done."
+
+
+# Products
+
+#
