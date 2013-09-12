d30c06f59799d71bc22d70b5116c11af03e753c9
Fernando Nino <nino.fernando@gmail.com>
2013-09-12 11:54:50 -0300

    Hobbies and occupation saved to MongoDB when creating a new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < GenericUser
+  attr_accessible :hobbies, :occupation
+  before_save :hobbies_to_array
+
   has_many :whishlist, class_name: 'Product'
   embeds_many :gifts
   field :hobbies, type: Array
+  field :occupation, type: String
+
+  private
+
+    def hobbies_to_array
+        self.hobbies = self.hobbies.split(',')
+    end
+
 end
