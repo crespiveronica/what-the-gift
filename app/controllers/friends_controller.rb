b54fbe07896f1478823bb9d22d60cde0f061e60d
Verónica Crespi <v@v-HP-Pavilion-dv4-Notebook-PC.(none)>
2013-10-06 18:11:18 -0300

    Modificando FriendsController para que solo que ahí la lógica de amistadZ

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,13 +1,5 @@
 class FriendsController < ApplicationController
 
-  def index
-    @friends = current_user.friends
-  	render 'friends/index', layout: 'myfriends'
-  end
-
-  def search
-  end
-
   def requests
   	@pending_requests = current_user.pending_requests
     render 'friends/requests', layout: 'myfriends'
@@ -19,14 +11,14 @@ class FriendsController < ApplicationController
     friend = User.find_by_id params[:id]
     request.friend = friend
     request.save
-    redirect_to action: 'show', :id => params[:id]
+    redirect_to action: 'users#show', :id => params[:id]
   end
 
   def accept
     request = FriendRequest.find(params[:id])
     request.accepted = true
     request.save
-    redirect_to action: 'index'
+    redirect_to action: 'users#friends'
   end
 
   def unfriend    
@@ -34,30 +26,12 @@ class FriendsController < ApplicationController
     friend = request.owner = current_user ? request.owner : request.friend
     request.delete
     flash[:success] = "Se ha eliminado a #{friend.full_name} de sus amigos"
-    redirect_to action: 'index'
+    redirect_to action: 'users#friends'
   end
 
   def pending
   	render 'friends/pending', layout: 'myfriends'
   end
 
-  def show
-    @friend = User.find_by_id params[:id]
-    @pending_friends = FriendRequest.friends_or_pending(current_user, @friend)
-    @request = FriendRequest.find_by_friends(current_user, @friend)
-    render 'friends/show', layout: 'friend'
-  end
-
-  def makeAGift
-    @friend = User.find_by_id params[:id]
-    @products = @friend.wishlist + @friend.recommended
-    @products = @products.uniq
-    render 'friends/makeagift', layout: 'friend'
-  end
-
-  def gifts
-    @friend = User.find_by_id params[:id]
-    render 'friends/gifts', layout: 'friend'
-  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,4 +35,32 @@ class UsersController < ApplicationController
   def destroy
   end
 
+  def friends
+    @friends = current_user.friends
+    render 'users/friends', layout: 'myfriends'
+  end
+  
+  def show_friend
+    @friend = User.find_by_id params[:id]
+    @pending_friends = FriendRequest.friends_or_pending(current_user, @friend)
+    @request = FriendRequest.find_by_friends(current_user, @friend)
+    render 'users/show_friend', layout: 'friend'
+  end
+
+  def search
+  end
+ 
+ def makeAGift
+    @friend = User.find_by_id params[:id]
+    @products = @friend.wishlist + @friend.recommended
+    @products = @products.uniq
+    render 'users/makeagift', layout: 'friend'
+  end
+
+  def gifts
+    @friend = User.find_by_id params[:id]
+    render 'friends/gifts', layout: 'friend'
+  end
+
+
 end
