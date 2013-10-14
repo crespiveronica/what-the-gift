af6e0d8c12321f6bfb2d5866c28270dae6a420d5
Fernando Nino <nino.fernando@gmail.com>
2013-10-14 19:30:27 -0300

    Correcion de typo en relacion has_many

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,14 @@
 class Product
   include Mongoid::Document
   include Mongoid::Search
-  
+
   field :name, type: String
   field :description, type: String
   field :brand, type: String
   field :approved, type: Boolean
 
   has_and_belongs_to_many :categories
-  has_many :selling_producs
+  has_many :selling_products
 
   search_in :brand, :name, :descripcion, :categories => :name
 
