2b68e8d387ad245c3ddea8f3efca14732ee72618
Verónica Crespi <v@v-HP-Pavilion-dv4-Notebook-PC.(none)>
2013-11-04 06:48:58 -0300

    Agregando filtro para categorías del menu

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,12 +3,15 @@ class Category
 
   field :name, type: String
   field :description, type: String
+  field :show_in_menu, type: Boolean, default: false
 
   has_and_belongs_to_many :products
 
   validates :name, uniqueness: {case_sensitive: false}
 
   default_scope order_by :name => :asc
+
+  scope :menu, where(show_in_menu: true)
 
   def self.for_admin
   	unscoped.order_by :name => :asc
