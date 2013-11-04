cc9d28677f14545c579127dca60a8cb9eccc6a5e
Joaquin Perez Bay <joaquinperezbay@gmail.com>
2013-11-03 22:41:06 -0300

    categorias agregadas

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,84 +19,174 @@ banned_reason = "none"
 
 # Cración de Categorías (Category)
 #
-
-# Accesorios para vehiculos
 accesorios_vehiculos_category = Category.new(name: 'Accesorios para vehículos', description: 'Accesorios para autos, motos, audio, tuning')
-accesorios_autos_category = Category.new(name: 'Accesorios para autos', description: 'Accesorios para el interior del auto, Accesorios para el exterior del auto, Repuestos para autos')
-accesorios_autos_interior_category = Category.new(name: 'Accesorios para el interior del auto', description: '')
-accesorios_autos_exterior_category = Category.new(name: 'Accesorios para el exterior del auto', description: '')
 repuestos_autos_category = Category.new(name: 'Accesorios para autos', description: '')
 accesorios_motos_category = Category.new(name: 'Accesorios para motos', description: 'Indumentaria para motos, Cascos para motos, Lingas y candados para motos, Respuestos para motos')
 indumentaria_motos_category = Category.new(name: 'Indumentaria para motos', description: '')
-cascos_motos_category = Category.new(name: 'Cascos para motos', description: '')
-lingas_motos_category = Category.new(name: 'Lingas y candados para motos', description: '')
 repuestos_motos_category = Category.new(name: 'Respuestos para motos', description: '')
 audio_vehiculos_category = Category.new(name: 'Audio para vehículos', description: 'Stereos con CD, Stereos con MP3, Stereos con USB, Subwoofers, Cargadores de MP3, Cargadores de celulares')
-mp3_vehiculos_category = Category.new(name: 'Stereos con MP3', description: '')
-usb_vehiculos_category = Category.new(name: 'Stereos con USB', description: '')
-cd_vehiculos_category = Category.new(name: 'Stereos con CD', description: '')
-subwoofer_vehiculos_category = Category.new(name: 'Subwoofers', description: '')
-carg_mp3_vehiculos_category = Category.new(name: 'Cargadores de MP3', description: '')
-carg_cel_vehiculos_category = Category.new(name: 'Cargadores de celulares', description: '')
-tuning_category = Category.new(name: 'Tuning para autos', description: '')
 volantes_category = Category.new(name: 'Volantes para autos', description: '')
-cromados_category = Category.new(name: 'Cromados para autos', description: '')
 polarizado_category = Category.new(name: 'Polarizado para autos', description: '')
-
 mascotas_category = Category.new(name: 'Mascotas', description: 'Aves, conejos, gatos, peces, perros, roedores')
 aves_category = Category.new(name: 'Aves', description: 'Alimento para aves, Jaulas para aves')
-alimento_aves_category = Category.new(name: 'Alimento para aves', description: '')
-jaula_aves_category = Category.new(name: 'Jaula para aves', description: '')
 conejos_category = Category.new(name: 'Conejos', description: 'Conejos, Jaulas para conejos')
-jaula_conejos_category = Category.new(name: 'Jaulas para conejos', description: '')
 gatos_category = Category.new(name: 'Gatos', description: 'Gatos, Accesorios para gatos, Alimento para gatos, Juguetes para gatos, Cuchas y transportadoras para gatos, Piedas sanitarias para gatos')
-accesorios_gatos_category = Category.new(name: 'Accesorios para gatos', description: '')
-alimento_gatos_category = Category.new(name: 'Alimento para gatos', description: '')
-juguetes_gatos_category = Category.new(name: 'Juguetes para gatos', description: '')
-cuchas_gatos_category = Category.new(name: 'Cuchas y transportadoras para gatos', description: '')
-piedras_gatos_category = Category.new(name: 'Piedras sanitarias para gatos', description: '')
 peces_category = Category.new(name: 'Peces', description: 'Peces, Accesorios para peceras, Peceras, Alimento para peces')
-accesorios_peces_category = Category.new(name: 'Accesorios para peces', description: '')
-peceras_category = Category.new(name: 'Peceras', description: '')
-alimento_peces_category = Category.new(name: 'Alimento para peces', description: '')
 perros_category = Category.new(name: 'Perros', description: 'Perros, Alimento para perros, Correas para perros, Juguetes para perros, Adiestramiento para perros, Vestimenta para perros')
-alimento_perros_category = Category.new(name: 'Alimento para perros', description: '')
-correas_perros_category = Category.new(name: 'Correas para perros', description: '')
-juguetes_perros_category = Category.new(name: 'Juguetes para perros', description: '')
-adiestramiento_perros_category = Category.new(name: 'Adiestramiento para perros', description: '')
-vestimenta_perros_category = Category.new(name: 'Vestimenta para perros', description: '')
 roedores_category = Category.new(name: 'Roedores', description: 'Cobayos, Hámsters')
-cobayos_category = Category.new(name: 'Cobayos', description: '')
-hamsters_category = Category.new(name: 'Hámsters', description: '')
-
-_category = Category.new(name: '', description: '')
-_category = Category.new(name: '', description: '')
-_category = Category.new(name: '', description: '')
-_category = Category.new(name: '', description: '')
-_category = Category.new(name: '', description: '')
-
-
-deportes_category = Category.new(name: 'Deportes', description: 'Productos para entrenamiento y fitness')
+antiguedades_category = Category.new(name: 'Antigüedades', description: 'Muebles, ornamentos, cristaleria, cámaras de fotos')
+audio_antiguo_category = Category.new(name: 'Audio antiguo', description: 'Discos de pasta, grabadores, radios, tocadiscots, vitrolas')
+cristaleria_antigua_category = Category.new(name: 'Cristaleria antigua', description: 'Botellas, ceniceros, centros de mesa, copas, floreros, fruteras, jarras, vasos')
+camaras_antiguas_category = Category.new(name: 'Cámaras antiguas', description: 'Cámaras de fotos antiguas')
+iluminacion_antigua_category = Category.new(name: 'Iluminación antigua', description: 'Apliques, arañas, cadelabros, faroles, plafones, veladores')
+juguetes_antiguos_category = Category.new(name: 'Juguetes antiguos', description: 'Animalitos y peluches, armas de juguete, juegos, muebles para muñecas, muñecas de porcelana, robots, soldaditos, trenes, vehículos, yoyos y trompos')
+muebles_antiguos_category = Category.new(name: 'Muebles antiguos', description: 'Aparadores, banquetas, bares, bargueños, baúles, bibliotecas, cómodas, consolas, cristaleros, mesas, percheros, puertas y herrajes, repisas, roperos, sillas, sillones, vitrinas')
+maquinas_de_escribir_antiguas_category = Category.new(name: 'Máquinas de escribir antiguas', description: 'Máquinas de escribir antiguas')
 libros_category = Category.new(name: 'Libros', description: 'Novelas, ciencia, comics')
-peliculas_category = Category.new(name: 'Películas', description: 'De Chaplin a Avatar')
-autos_category = Category.new(name: 'Autos', description: '')
-computacion_category = Category.new(name: 'Computacion', description: 'Laptops, de escritorio y todos sus accesorios')
-celulares_category = Category.new(name: 'Celulares', description: 'Dumbphones, smartphones, cargadores y fundas')
 juegos_category = Category.new(name: 'Juegos', description: 'TEG, Juego de la Vida, Monopoly y otros clásicos')
-instrumentos_category = Category.new(name: 'Instrumentos', description: 'Guitarras, bajos, amplificadores y cuerdas')
+arte_artesanias_category = Category.new(name: 'Arte y Artesanías', description: 'Artesanías, esculturas, libros, materiales para tatuajes, souvenirs')
+materiales_para_tatuajes_category = Category.new(name: 'Materiales para tatuajes', description: 'Tintas, equipos de tatuaje, Stencils, Kits')
+souvenirs_category = Category.new(name: 'Souvenirs', description: '15 años, bautimos, casamientos, comuniones, cumpleaños infantiles, empresariales, nacimientos')
+bebes_category = Category.new(name: 'Bebés', description: 'Alimentos, andadores, arneses para autos, artículos de bebés para baño, embarazadas, cochecitos, juguetes, pañales y pañaleras, recuerdos, ropa, seguridad, sillas de comer')
+articulos_embarazadas_category = Category.new(name: 'Artículos para embarazadas', description: 'Todo para la mujer embarazada')
+juguetes_bebes_category = Category.new(name: 'Juguetes para bebés', description: 'Cubos, gimnasios, libros didácticos, mantas, mecedoras, hamacas, mesas y sillas, móviles, musicales, muñecos, ositos y peluches, pelotas, sonajeros')
+seguridad_bebes_category = Category.new(name: 'Seguridad para bebés', description: 'Arneses, baby calls, mochilas y porta bebés, mostuiteros, pisos de goma, puertas de seguridad')
+camaras_accesorios_category = Category.new(name: 'Cámaras y accesorios', description: 'Cámaras digitales, polaroid y a rollo, memorias, baterías, pilas y cargadores.')
+accesorios_camaras_category = Category.new(name: 'Accesorios para cámaras', description: 'Accesorios ópticos, bolsos, fundas, correas, cables, controles remotos, displayas, films protectores, filtros flashes e iluminación, toómetros, lectores de tarjetas, rollos, trípodes, monopies')
+laboratorios_category = Category.new(name: 'Laboratorios para revelado', description: 'Kioskos digitales, mini labs, papéles fotográficos')
+portarretratos_albumes_category = Category.new(name: 'Portarretratos y álbumes', description: 'Álbumes, llaveros, portarretratos convecionales y digitales')
+videocamaras_category = Category.new(name: 'Videocámaras', description: 'Cámaras filmadoras análogicas y digitales, profesionales y convencionales')
+accesorios_celulares_category = Category.new(name: 'Accesorios para celulares', description: 'Adaptadores, amplificadores, antenas, baterias, cables, carcasas, cargadores, colgantes y correas, holders y fundas, manos libres, memorias, parlantes, protectores de pantallas, soportes para autos')
+celulares_category = Category.new(name: 'Celulares', description: 'Todas las marcas')
+telefonia_IP_category = Category.new(name: 'Telefonía IP', description: 'Gateways, planes, routers')
+telefonia_convencional_category = Category.new(name: 'Telefonía convencional', description: 'Aceesorios, caller ID, contestadores, telefónos con cables, teléfonos inalámbricos')
+tablets_category = Category.new(name: 'Tablets', description: 'MAC, Hacer, Archos, BlackBerry playbook, Kindle, Motorola, Samsung, Titan')
+pcs_portatiles_category = Category.new(name: 'PC y Portátiles', description: 'Con y sin monitor, portátiles notebooks y netbooks')
+software_category = Category.new(name: 'Software', description: 'Aplicaciones comerciales, juegos, seguridad, antivirus')
+hardware_category = Category.new(name: 'Hardware', description: 'Memorias RAM, procesadores, lectoras (CD/DVD/Bluray), PenDrives, fuentes, UPS, estabilizadores, impresoras')
+consolas_videojuegos_retro_category = Category.new(name: 'Consolas y juegos retro', description: 'Consolas y juegos retro')
+consolas_category = Category.new(name: 'Consolas de videojuegos', description: 'GameCube, Playstation 1 2 y 3, Nintendo 3DS, Nintendo Ds, Xbox y Xbox 360, Wii, GameBoy')
+joysticks_category = Category.new(name: 'Joysticks', description: '')
+videojuegos_category = Category.new(name: 'Videojuegos', description: 'GameCube, Playstation 1 2 y 3, Nintendo 3DS, Nintendo Ds, Xbox y Xbox 360, Wii')
+bodegas_category = Category.new(name: 'Bodegas', description: 'Muebles, wine sellars')
+bebidas_alcoholicas_category = Category.new(name: 'Bebidas alcoholicas', description: 'Bebidas blancas, vinos, fernets, licores, champagnes, whiskys')
+energizantes_category = Category.new(name: 'Energizantes', description: '')
+cigarros_pipas_category = Category.new(name: 'Cigarros y pipas', description: 'Cigarros, habanos, humidores, narguiles, pipas')
+deportes_category = Category.new(name: 'Accesorios para deportes', description: 'Accesorios para aerobics, fitness, artes marciales, básquet, ciclismo, fútbol, fútbol americano, gold, handball, Hockey, tennis, paddle, squash, pulsómetros, cronómetros')
+indumentaria_deportiva_category = Category.new(name: 'Indumentaria Deportiva', description: 'Indumentaria para aerobics, fitness, artes marciales, básquet, ciclismo, fútbol, fútbol americano, gold, handball, Hockey, tennis, paddle, squash, pulsómetros, cronómetros')
+calazado_deportivo_category = Category.new(name: 'Calzado deportivo', description: 'Calzado para aerobics, fitness, artes marciales, básquet, ciclismo, fútbol, fútbol americano, gold, handball, Hockey, tennis, paddle, squash, pulsómetros, cronómetros')
+accesorios_audio_category = Category.new(name: 'Accesorios para audio', description: 'Cables, adaptadores, antenas, conversores')
+equipos_audio_category = Category.new(name: 'Equipos de audio', description: 'Reproductores, equipos para DJ, home theater')
+accesorios_video_category = Category.new(name: 'Accesorios para video', description: 'Lentes 3D')
+equipos_video_category = Category.new(name: 'Equipos de video', description: 'Lectograbadores CD, DVD, Bluray, videocaseteras')
+televisores_category = Category.new(name: 'Televisores', description: '')
+gps_category = Category.new(name: 'GPS', description: '')
+entradas_eventos_category = Category.new(name: 'Entradas para eventos', description: 'Eventos deportivos, eventos finalizados, recitales, teatro')
+instrumentos_musicales_category = Category.new(name: 'Intrumentos musicales', description: 'Bajos, instrumentos de percusión, instrumentos de cuerdas, instrumentos de viento')
+accesorios_musica_category = Category.new(name: 'Accesorios para música', description: 'Amplificadores, consolas de sonido, efectos de sonido, micrófonos, pies, parlantes')
+guitarras_category = Category.new(name: 'Guitarras', description: 'Criollas, eléctricas')
+pianos_category = Category.new(name: 'Pianos', description: 'Teclados y pianos')
+baterías_category = Category.new(name: 'Baterías', description: '')
+juguetes_mujer_category = Category.new(name: 'Juguetes para mujer', description: '')
+juguetes_hombre_category = Category.new(name: 'Juguetes para hombre', description: '')
+peliculas_series_category = Category.new(name: 'Películas series', description: 'Merchandising y DVD/Bluray')
+musica_pop_category = Category.new(name: 'Música Pop', description: '')
+musica_rock_category = Category.new(name: 'Música Rock', description: '')
+musica_punk_category = Category.new(name: 'Música Punk', description: '')
+musica_folk_category = Category.new(name: 'Música Folk', description: '')
+musica_electronica_category = Category.new(name: 'Música Electrónica', description: '')
+musica_raggae_category = Category.new(name: 'Música Raggae', description: '')
+salud_category = Category.new(name: 'Salud', description: 'Cuidados del cuerpo, dietas')
+relajacion_category = Category.new(name: 'Relajación', description: 'Spa, masajes')
+ropa_mujer_category = Category.new(name: 'Ropa de mujer', description: '')
+ropa_hombre_category = Category.new(name: 'Ropa de hombre', description: '')
+
 
 #
 # Guardar Categorías
 #
-deportes_category.save
-libros_category.save
-peliculas_category.save
+
+accesorios_vehiculos_category.save
+repuestos_autos_category.save
+accesorios_motos_category.save
+indumentaria_motos_category.save
+repuestos_motos_category.save
+audio_vehiculos_category.save
+volantes_category.save
+polarizado_category.save
 mascotas_category.save
-autos_category.save
-computacion_category.save
-celulares_category.save
+aves_category.save
+conejos_category.save
+gatos_category.save
+peces_category.save
+perros_category.save
+roedores_category.save
+antiguedades_category.save
+audio_antiguo_category.save
+cristaleria_antigua_category.save
+camaras_antiguas_category.save
+iluminacion_antigua_category.save
+juguetes_antiguos_category.save
+muebles_antiguos_category.save
+maquinas_de_escribir_antiguas_category.save
+libros_category.save
 juegos_category.save
-instrumentos_category.save
+arte_artesanias_category.save
+materiales_para_tatuajes_category.save
+souvenirs_category.save
+bebes_category.save
+articulos_embarazadas_category.save
+juguetes_bebes_category.save
+seguridad_bebes_category.save
+camaras_accesorios_category.save
+accesorios_camaras_category.save
+laboratorios_category.save
+portarretratos_albumes_category.save
+videocamaras_category.save
+accesorios_celulares_category.save
+celulares_category.save
+telefonia_IP_category.save
+telefonia_convencional_category.save
+tablets_category.save
+pcs_portatiles_category.save
+software_category.save
+hardware_category.save
+consolas_videojuegos_retro_category.save
+consolas_category.save
+joysticks_category.save
+videojuegos_category.save
+bodegas_category.save
+bebidas_alcoholicas_category.save
+energizantes_category.save
+cigarros_pipas_category.save
+deportes_category.save
+indumentaria_deportiva_category.save
+calazado_deportivo_category.save
+accesorios_audio_category.save
+equipos_audio_category.save
+accesorios_video_category.save
+equipos_video_category.save
+televisores_category.save
+gps_category.save
+entradas_eventos_category.save
+instrumentos_musicales_category.save
+accesorios_musica_category.save
+guitarras_category.save
+pianos_category.save
+baterías_category.save
+juguetes_mujer_category.save
+juguetes_hombre_category.save
+peliculas_series_category.save
+musica_pop_category.save
+musica_rock_category.save
+musica_punk_category.save
+musica_folk_category.save
+musica_electronica_category.save
+musica_raggae_category.save
+salud_category.save
+relajacion_category.save
+ropa_mujer_category.save
+ropa_hombre_category.save
 
 #
 # Creación de Productos (Product)
