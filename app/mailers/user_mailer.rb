4ecb6e45e7a394b0a3eb3e221dd2f9333b2f31fa
slifszyc <sacha.lifszyc@gmail.com>
2013-11-05 23:03:16 -0300

    email header in all emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,34 +3,41 @@ class UserMailer < ActionMailer::Base
 
   def signup_email(user)
     @user = user
+    @url = 'http://what-the-gift.com'
+    @headerImage = @url + '/assets/email-header.png'
     @link = "http://what-the-gift.com/confirm/" + @user.id + "/" + @user.signup_token
     mail(to: @user.email, subject: 'Bienvenido a What the Gift?!')
   end
 
   def new_email_email(user)
     @user = user
+    @url = 'http://what-the-gift.com'
+    @headerImage = @url + '/assets/email-header.png'
     @link = "http://what-the-gift.com/confirm-mail/" + @user.id + "/" + @user.signup_token
     mail(to: @user.new_email, subject: 'Confirme su nuevo mail')
   end
 
   def forgotten_password(user)
   	@user = user
+    @url = 'http://what-the-gift.com'
+    @headerImage = @url + '/assets/email-header.png'
   	mail(to: @user.email, subject: 'Nueva password')
   end
 
   def inform_state(user)
     @user = user
+    @url = 'http://what-the-gift.com'
+    @headerImage = @url + '/assets/email-header.png'
     mail(to: @user.email, subject: 'Se ha cambiado su estado')
   end
 
   def birthday_notification(user)
     @user = user
     @friends = user.birthday_friends
-    url = 'http://what-the-gift.com'
-    @link_friend = url + "/friends/"
-    @link_gift = url + "/products/"
-    @url = url 
-    @headerImage = url + '/assets/email-header.png'
+    @url = 'http://what-the-gift.com'
+    @headerImage = @url + '/assets/email-header.png'
+    @link_friend = @url + "/friends/"
+    @link_gift = @url + "/products/"
     mail(to: user.email, subject: "Se acercan nuevos cumpleaños")
   end
 
